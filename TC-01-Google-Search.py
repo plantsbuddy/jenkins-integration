f4b549cf5dfc9019c14c19272fed28b04a917313
plantsbuddy <114995069+plantsbuddy@users.noreply.github.com>
2023-06-10 03:51:41 +0500

    Update TC-01-Google-Search.py

--- TC-01-Google-Search.py
+++ TC-01-Google-Search.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # Wait for the search results to load
-time.sleep(3)
+time.sleep(21)
 
 # Print a success message if the assertion passes
 print("Test passed! Google search for 'selenium' returned the expected results.")
